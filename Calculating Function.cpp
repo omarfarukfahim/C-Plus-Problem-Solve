5fd43bac83307faeffad0c088fea8002fda07841
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-02-09 23:58:26 +0600

    new

--- Calculating Function.cpp
+++ Calculating Function.cpp
@@ -1,14 +1,3 @@
-/**
- * Problem: A. Calculating Function
- * Analysis:
- * The function f(n) represents an alternating sum.
- * Simulating the sum is too slow for n = 10^15.
- * We derived a pattern:
- * - If n is even, f(n) = n / 2.
- * - If n is odd, f(n) = -(n + 1) / 2.
- * We use 'long long' to handle large integers.
- */
-
 #include <iostream>
 
 using namespace std;
@@ -18,17 +7,13 @@ void solve() {
     if (!(cin >> n)) return;
 
     if (n % 2 == 0) {
-        // Even case
         cout << n / 2 << "\n";
     } else {
-        // Odd case
-        // Using integer division, -(n+1)/2 works perfectly
         cout << -(n + 1) / 2 << "\n";
     }
 }
 
 int main() {
-    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
