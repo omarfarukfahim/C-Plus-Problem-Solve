3262d0aa00ed88af31982258ec36db4d2c410ead
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-02-10 00:01:35 +0600

    new

--- Magnets.cpp
+++ Magnets.cpp
@@ -1,12 +1,3 @@
-/**
- * Problem: A. Magnets
- * Analysis:
- * We iterate through the sequence of magnets.
- * Whenever the current magnet is different from the previous one (e.g., changing from "10" to "01"),
- * it means like poles are touching (repulsion), starting a new group.
- * If they are the same ("10" and "10"), they attract and stay in the same group.
- */
-
 #include <iostream>
 #include <string>
 
@@ -23,16 +14,13 @@ void solve() {
         string curr;
         cin >> curr;
 
-        // If it's the first magnet, it starts the first group.
         if (i == 0) {
             groups++;
         } 
-        // If current magnet is different from previous, a new group starts.
         else if (curr != prev) {
             groups++;
         }
         
-        // Update previous to current for the next iteration
         prev = curr;
     }
 
@@ -40,7 +28,6 @@ void solve() {
 }
 
 int main() {
-    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
