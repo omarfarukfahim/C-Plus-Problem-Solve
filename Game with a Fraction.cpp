cd54aeb42c299f44c2ecb9526847aacff5145829
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-02-11 21:18:32 +0600

    new

---
+++ Game with a Fraction.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    ll p, q;
+    if (!(cin >> p >> q)) return;
+
+    if (3 * p == 2 * q) {
+        cout << "Bob" << "\n";
+        return;
+    }
+
+    ll K = min(p / 2, q / 3);
+
+    if (K == 0) {
+        cout << "Alice" << "\n";
+        return;
+    }
+
+    ll X = p - 2 * K;
+    ll Y = q - 3 * K;
+
+    if (X == Y) {
+        cout << "Bob" << "\n";
+    } else {
+        cout << "Alice" << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
