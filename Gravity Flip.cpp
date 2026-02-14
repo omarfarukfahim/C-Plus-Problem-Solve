c6b2262679d08f4b9428e79bc24721d305d411cc
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-02-14 23:32:54 +0600

    new

---
+++ Gravity Flip.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+void solve() {
+    int n;
+    if (!(cin >> n)) return;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
