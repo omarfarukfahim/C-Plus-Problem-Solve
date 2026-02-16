8d480a557a493ba0a9846115465228f95cf8fd55
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-02-16 23:42:21 +0600

    update

--- Registration system.cpp
+++ Registration system.cpp
@@ -1,13 +1,3 @@
-/**
- * Problem: C. Registration System
- * Analysis:
- * We need to track the frequency of each username request.
- * We can use a map to store the 'next available suffix' for every name.
- * - If a name 's' is not in the map, print "OK" and set map[s] = 1.
- * - If 's' is in the map, print 's' + map[s], and then increment map[s].
- * This approach effectively assigns s1, s2, s3... to subsequent duplicates.
- */
-
 #include <iostream>
 #include <string>
 #include <map>
@@ -24,24 +14,18 @@ void solve() {
     while (n--) {
         cin >> s;
         
-        // If the name is not in our database
         if (db.count(s) == 0) {
             cout << "OK" << "\n";
-            // Initialize the counter for this name to 1
-            // (meaning the next duplicate will be name1)
             db[s] = 1;
         } 
         else {
-            // Name exists, generate a new one using the stored counter
             cout << s << db[s] << "\n";
-            // Increment the counter for the next time this name is requested
             db[s]++;
         }
     }
 }
 
 int main() {
-    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
