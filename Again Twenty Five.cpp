77b9ca1ff48b64939dcc031073bac02d95caa97b
Omar Faruk Fahim <omarfaruqfahim007@gmail.com>
2026-03-03 23:48:51 +0600

    new

---
+++ Again Twenty Five.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    string n;
+    
+    if (cin >> n) {
+        cout << "25\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
